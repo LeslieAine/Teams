38b8796b1953127414dbf910c6d5a2f81e780353
LeslieAine <aineleslie@gmail.com>
2023-08-29 13:56:26 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  as :user do
+    put '/user/confirmation' => 'confirmations#update', via: :patch, :as => :update_user_confirmation
+  end
+
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmation: 'confirmations'
+  }
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
