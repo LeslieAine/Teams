1293eec49aba37e7452c4f10be9cee2f0282087f
LeslieAine <aineleslie@gmail.com>
2023-09-05 21:17:11 +0300

    Update teams controller and model

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :users
 
-  accepts_nested_attributes_for :user, allow_destroy: true
+  accepts_nested_attributes_for :users, allow_destroy: true
 end
